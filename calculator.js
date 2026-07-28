039d137aba2a7528c5e2e5998cff4c272fa6a5be
Mark Lhester Pile <pilemarklhester028@gmail.com>
2026-07-29 07:07:49 +0800

    script(calculator.js): added the functionality to enable the calculator

---
+++ calculator.js
@@ -0,0 +1,34 @@
+const selectors = {
+  inputField: document.getElementById("result-container"),
+  mainButtonDiv: document.querySelector(".btn-main-container"),
+};
+
+let currentInput = "";
+
+selectors.mainButtonDiv.addEventListener("click", (event) => {
+  const triggeredButton = event.target.closest(".btn");
+  if (!triggeredButton) return;
+  selectors.inputField.value = triggeredButton.getAttribute("data-input");
+
+  const value = triggeredButton.getAttribute("data-input");
+
+  if (value === "AC") {
+    currentInput = "";
+  } else if (value === "DEL") {
+    currentInput = currentInput.slice(0, -1);
+  } else if (value === "=") {
+    try {
+      const expression = currentInput.replace(/%/g, "/100");
+      //g = replace all occurences
+      currentInput = String(eval(expression));
+    } catch {
+      currentInput = "Error";
+    }
+  } else if (value === "%") {
+    currentInput = "%";
+  } else {
+    currentInput += value;
+  }
+
+  selectors.inputField.value = currentInput;
+});
